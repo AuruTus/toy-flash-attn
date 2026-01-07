a17b77f3cf349da8092f0466b0600103f773ff10
AuruTus <emslhy@hotmail.com>
2026-01-07 22:10:29 +0800

    feat: data movement
    Signed-off-by: AuruTus <emslhy@hotmail.com>

---
+++ csrc/flash_attn_v2/include/load_store.cuh
@@ -0,0 +1,238 @@
+#pragma once
+
+#include <cuda.h>
+#include "ptx_function.cuh"
+
+struct LDSTCommon {
+    const bool swizzled;
+    const bool async_copy;
+};
+
+struct TileLayout {
+    const int row_fragments;
+    const int col_fragments;
+};
+
+// LD/ST configuration for a block (Q, K, V, or O) from GMEM to SMEM
+// and vice versa, and also loading from SMEM to RF
+struct TensorLDSTConfig {
+    // Tile layout for SMEM and RF
+    const TileLayout GSM;
+    const TileLayout RF;
+    const LDSTCommon Common;
+
+    // Block specific properties
+    const bool transposed;
+    const int block_size;
+    const int smem_cols;
+
+    // number of rows a warp in a thread block independently loads/stores,
+    // which is equivalent to GSM.row_fragments * 8
+    const int warp_ldst_rows;
+    // Whether or not the warp will compute over the entire block.
+    // This is `false` for (Q & O & S) and `true` for (K & V)
+    const bool compute_over_entire_blocks;
+
+    const bool load_entire_block_into_rf;
+    const int mma_load_stages;
+};
+
+constexpr int ROWS_PER_FRAGMENT = 8;
+constexpr int COLS_PER_FRAGMENT = 8;
+constexpr int GSM_LDST_ROWS_PER_ITER = 4;
+constexpr int BYTES_PER_VEC4_ACCESS = 16;
+
+constexpr int ELEMS_PER_VEC4_ACCESS = 8;
+
+template <typename T, typename value_t>
+concept gmem_smem_op = requires(value_t* gmem, value_t* smem) {
+    { T::run(gmem, smem) } -> std::same_as<void>;
+};
+
+template <typename T>
+struct GM2SM_async {
+    __device__ static void run(T* gmem, T* smem) {
+        cp_async<BYTES_PER_VEC4_ACCESS / sizeof(T)>(smem, gmem);
+    }
+};
+
+template <typename T>
+struct GM2SM {
+    __device__ static void run(T* gmem, T* smem) {
+        reinterpret_cast<uint4*>(smem)[0] = reinterpret_cast<uint4*>(gmem)[0];
+    }
+};
+
+template <typename T>
+struct SM2GM {
+    __device__ static void run(T* gmem, T* smem) {
+        reinterpret_cast<uint4*>(gmem)[0] = reinterpret_cast<uint4*>(smem)[0];
+    }
+};
+
+template <
+    typename OP,
+    TensorLDSTConfig CFG,
+    typename value_t,
+    typename index_t = int64_t,
+    int WARP_SIZE = 32>
+    requires gmem_smem_op<OP, value_t>
+__forceinline__ __device__ void copy_block_GSM(
+    value_t* gmem,
+    value_t* smem,
+    index_t gmem_seq_stride,
+    const int lane_id
+) {
+    constexpr auto n_row_iters =
+        CFG.GSM.row_fragments * ROWS_PER_FRAGMENT / GSM_LDST_ROWS_PER_ITER;
+
+    constexpr auto col_fragments_per_iter = WARP_SIZE / GSM_LDST_ROWS_PER_ITER;
+    constexpr auto col_fragments_per_row = CFG.smem_cols / COLS_PER_FRAGMENT;
+
+    const auto thread_row = lane_id / col_fragments_per_iter;
+    const auto thread_col_fragment = lane_id % col_fragments_per_iter;
+
+    [[unroll]]
+    for (int r = 0; r < n_row_iters; ++r) {
+        const auto curr_row = r * GSM_LDST_ROWS_PER_ITER + thread_row;
+        [[unroll]]
+        for (int c = 0; c < col_fragments_per_row;
+             c += col_fragments_per_iter) {
+            const auto col_fragment = c + thread_col_fragment;
+
+            OP::run(
+                &gmem
+                    [curr_row * gmem_seq_stride +
+                     col_fragment * COLS_PER_FRAGMENT],
+                &smem
+                    [curr_row * CFG.smem_cols +
+                     col_fragment * COLS_PER_FRAGMENT]
+            );
+        }
+    }
+}
+
+template <TensorLDSTConfig CFG, typename value_t, int WARP_SIZE = 32>
+__forceinline__ __device__ void copy_warp_fragment_SM2RF(
+    uint32_t (&regs)[CFG.RF.row_fragments][CFG.RF.col_fragments],
+    value_t* smem,
+    const int lane_id,
+    const int col_fragment_offset = 0
+) {
+    constexpr auto row_fragments_per_iter = 2;
+    constexpr auto rows_per_iter = ROWS_PER_FRAGMENT * row_fragments_per_iter;
+
+    constexpr auto col_fragments = CFG.smem_cols / ELEMS_PER_VEC4_ACCESS;
+    constexpr auto col_fragments_per_iter = WARP_SIZE / rows_per_iter;
+
+    const auto thread_row = lane_id % rows_per_iter;
+    const auto thread_col_fragment = lane_id / rows_per_iter;
+
+    [[unroll]]
+    for (int r = 0; r < CFG.RF.row_fragments; r += row_fragments_per_iter) {
+        const auto curr_row = thread_row + r * ROWS_PER_FRAGMENT;
+        [[unroll]]
+        for (int c = 0; c < CFG.RF.col_fragments; c += col_fragments_per_iter) {
+            const auto smem_col_fragment =
+                thread_col_fragment + c + col_fragment_offset;
+
+            ldmatrix_x4(
+                &smem
+                    [curr_row * CFG.smem_cols +
+                     smem_col_fragment * ELEMS_PER_VEC4_ACCESS],
+                regs[r][c], regs[r + 1][c], regs[r][c + 1], regs[r + 1][c + 1]
+            );
+        }
+    }
+}
+
+template <TensorLDSTConfig CFG, typename value_t, int WARP_SIZE = 32>
+__forceinline__ __device__ void copy_warp_fragment_transposed_SM2RF(
+    uint32_t (&regs)[CFG.RF.row_fragments][CFG.RF.col_fragments],
+    value_t* smem,
+    const int lane_id,
+    const int row_fragment_offset = 0
+) {
+    constexpr auto row_fragments_per_iter = 2;
+    constexpr auto rows_per_iter = ROWS_PER_FRAGMENT * row_fragments_per_iter;
+
+    constexpr auto col_fragments = CFG.smem_cols / ELEMS_PER_VEC4_ACCESS;
+    constexpr auto col_fragments_per_iter = WARP_SIZE / rows_per_iter;
+
+    const auto thread_row = lane_id % rows_per_iter;
+    const auto thread_col_fragment = lane_id / rows_per_iter;
+
+    [[unroll]]
+    for (int r = 0; r < CFG.RF.row_fragments; r += row_fragments_per_iter) {
+        const auto curr_row =
+            thread_row + (r + row_fragment_offset) * ROWS_PER_FRAGMENT;
+        [[unroll]]
+        for (int c = 0; c < CFG.RF.col_fragments; c += col_fragments_per_iter) {
+            const auto smem_col_fragment = thread_col_fragment + c;
+
+            ldmatrix_x4_transpose(
+                &smem
+                    [curr_row * CFG.smem_cols +
+                     smem_col_fragment * ELEMS_PER_VEC4_ACCESS],
+                regs[c][r], regs[c + 1][r], regs[c][r + 1], regs[c + 1][r + 1]
+            );
+        }
+    }
+}
+
+template <TensorLDSTConfig CFG, typename value_t, int WARP_SIZE = 32>
+__forceinline__ __device__ void copy_warp_fragment_RF2SM(
+    uint32_t (&regs)[CFG.RF.row_fragments][CFG.RF.col_fragments],
+    value_t* smem,
+    const int lane_id
+) {
+    constexpr auto rows_per_iter = ROWS_PER_FRAGMENT;
+    constexpr auto col_fragments_per_iter = 1;
+    constexpr auto col_fragments = CFG.smem_cols / ELEMS_PER_VEC4_ACCESS;
+
+    constexpr auto elems_per_store = 2;
+    const auto thread_row = lane_id / 4;
+    const auto thread_inner_col = (lane_id % 4) * elems_per_store;
+
+    [[unroll]]
+    for (int r = 0; r < CFG.RF.row_fragments; ++r) {
+        const auto curr_row = r * rows_per_iter + thread_row;
+        [[unroll]]
+        for (int c = 0; c < CFG.RF.col_fragments; c += col_fragments_per_iter) {
+            const auto smem_col_fragment = c;
+            reinterpret_cast<uint32_t*>(
+                &smem
+                    [curr_row * CFG.smem_cols +
+                     (smem_col_fragment * ELEMS_PER_VEC4_ACCESS +
+                      thread_inner_col)]
+            )[0] = regs[r][c];
+        }
+    }
+}
+
+template <
+    typename value_t,
+    int M_fragments,
+    int N_fragments,
+    bool is_half = std::is_same_v<value_t, half>>
+__forceinline__ __device__ constexpr void convert_to_16_bit_dtype(
+    float (&src_float)[M_fragments][N_fragments * 2],
+    uint32_t (&dest_uint)[M_fragments][N_fragments]
+) {
+    using value2_t = std::conditional_t<is_half, half2, nv_bfloat162>;
+
+    auto src = reinterpret_cast<float2(&)[M_fragments][N_fragments]>(src_float);
+    auto dest =
+        reinterpret_cast<value2_t(&)[M_fragments][N_fragments]>(dest_uint);
+    [[unroll]]
+    for (int m = 0; m < M_fragments; ++m) {
+        [[unroll]]
+        for (int n = 0; n < N_fragments; ++n) {
+            if constexpr (is_half) {
+                dest[m][n] = __float22half2_rn(src[m][n]);
+            } else {
+                dest[m][n] = __float22bfloat162_rn(src[m][n]);
+            }
+        }
+    }
+}
---
+++ csrc/flash_attn_v2/include/tensor.cuh
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <type_traits>
+
+#include "load_store.cuh"
+
+template <typename value_t, int N>
+struct RFVector {
+    static constexpr auto size = N;
+    value_t regs[N];
+
+    __forceinline__ __device__ constexpr value_t& operator[](int idx) {
+        return regs[idx];
+    }
+};
+
+template <typename value_t, int n_copies, int row_fragments, int col_fragments>
+struct RFMatrix {
+    using storage_t = std::conditional_t<sizeof(value_t) == 4, float, uint32_t>;
+
+    static constexpr int regs_per_fragment = sizeof(value_t) / 2;
+    static constexpr int rows = row_fragments;
+    static constexpr int cols = col_fragments * regs_per_fragment;
+
+    storage_t regs[n_copies][rows][cols];
+
+    __forceinline__ __device__ constexpr auto data(const int stage = 0)
+        -> storage_t (&)[rows][cols] {
+        return reinterpret_cast<storage_t(&)[rows][cols]>(regs[stage]);
+    }
+
+    __forceinline__ __device__ constexpr void zero() {
+        [[unroll]]
+        for (int j = 0; j < rows; ++j) {
+            [[unroll]]
+            for (int k = 0; k < cols; ++k) {
+                this->regs[j][k] = 0;
+            }
+        }
+    }
+};
+
+template <
+    TensorLDSTConfig ldst,
+    typename value_t,
+    typename index_t = int64_t,
+    int WARP_SIZE = 32>
+struct MatrixLDST {
+    using matrix_storage_t = RFMatrix<
+        value_t,
+        ldst.mma_load_stages,
+        ldst.RF.row_fragments,
+        ldst.RF.col_fragments>;
+
+    using GM2SM_op = std::conditional_t<
+        ldst.Common.async_copy,
+        GM2SM_async<value_t>,
+        GM2SM<value_t>>;
+
+    using SM2GM_op = SM2GM<value_t>;
+
+    static constexpr int mma_load_stages = ldst.mma_load_stages;
+    static constexpr bool load_entire_block_into_rf =
+        ldst.load_entire_block_into_rf;
+    static constexpr bool transposed = ldst.transposed;
+
+    // Runtime properties
+    value_t* gmem_ptr;
+    index_t gmem_seq_stride;
+    // The location in memory used to load fragments from SMEM to RF
+    value_t* smem_srm_ptr;
+    // The location in memory that the warp writes to for Q, K, V from GMEM to
+    // SMEM and O for SMEM to GMEM
+    valut_t* smem_gsm_ptr;
+
+    const int lane_id;
+
+    matrix_storage_t storage;
+
+    __forceinline__ __device__ MatrixLDST(
+        value_t* gmem_block_ptr,
+        index_t gmem_seq_stride,
+        value_t* smem_ptr,
+    )
+        : lane_id(threadIdx.x % WARP_SIZE) {
+        const int warp_rank = threadIdx.x / WARP_SIZE;
+        const index_t warp_seq = ldst.warp_ldst_rows * warp_rank;
+
+        this->gmem_seq_stride = gmem_seq_stride;
+        this->gmem_ptr = gmem_block_ptr + warp_seq * gemem_seq_stride;
+        this->smem_gsm_ptr = smem_ptr + warp_seq * ldst.smem_cols;
+        this->smem_srm_ptr =
+            ldst.compute_over_entire_blocks ? smem_ptr : smem_gsm_ptr;
+    }
+
+    __forceinline__ __device__ constexpr void zero() { this->storage.zero(); }
+
+    __forceinline__ __device__ constexpr auto data(const int stage = 0) ->
+        typename matrix_storage_t::storage_t (&)[matrix_storage_t::rows]
+                                                [matrix_storage_t::cols] {
+        return this->storage.data(stage);
+    }
+
+    __forceinline__ __device__ constexpr void advance_gmem_block() {
+        this->gmem_ptr += ldst.block_size * this->gmem_seq_stride;
+    }
+
+    __forceinline__ __device__ constexpr void copy_GM2SM() {
+        copy_block_GSM<GM2SM_op, ldst, value_t, index_t, WARP_SIZE>(
+            this->gmem_ptr, this->smem_gsm_ptr, this->gmem_seq_stride,
+            this->lane_id
+        );
+    }
+
+    __forceinline__ __device__ constexpr void copy_SM2GM() {
+        copy_block_GSM<SM2GM_op, ldst, value_t, index_t, WARP_SIZE>(
+            this->gmem_ptr, this->smem_gsm_ptr, this->gmem_seq_stride,
+            this->lane_id
+        );
+    }
+
+    __forceinline__ __device__ constexpr void
+    copy_SM2RF(int stage = 0, int title_offset = 0) {
+        if constexpr (!this->transposed) {
+            copy_warp_fragment_SM2RF<ldst, value_t, WARP_SIZE>(
+                this->storage.data(stage), this->smem_srm_ptr, this->lane_id,
+                title_offset,
+            );
+        } else {
+            copy_warp_fragment_transposed_SM2RF<ldst, value_t, WARP_SIZE>(
+                this->storage.data(stage), this->smem_srm_ptr, this->lane_id,
+                title_offset,
+            );
+        }
+    }
+
+    __forceinline__ __device__ constexpr void copy_RF2SM() {
+        copy_warp_fragment_RF2SM<ldst, value_t, WARP_SIZE>(
+            this->smem_srm_ptr, this->storage.data(0), this->lane_id
+        );
+    }
+};
