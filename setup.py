c50d5c247bfcd5cc9c88c5c0790c169e8ef043b2
AuruTus <emslhy@hotmail.com>
2026-02-04 16:41:26 +0800

    refator: mark the project path
    Signed-off-by: AuruTus <emslhy@hotmail.com>

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""
+Minimal setup.py for backward compatibility.
+All configuration is in pyproject.toml.
+"""
+
+from setuptools import setup
+
+if __name__ == "__main__":
+    setup()
